b320b4a6557f793dafcb26c174fc7f0d44b1195e
Pavel Kovalenko <pavel@kovalenko.cz>
2019-11-29 22:56:50 +0100

    Remove checkout from NavMenu

--- src/components/Navigation/NavigationItems/NavigationItems.jsx
+++ src/components/Navigation/NavigationItems/NavigationItems.jsx
@@ -5,7 +5,6 @@ import styles from './NavigationItems.css'
 const NavigationItems = () => (
     <ul className={styles.NavigationItems}>
         <NavigationItem link='/' exact>Burger Builder</NavigationItem>
-        <NavigationItem link='/checkout'>Checkout</NavigationItem>
         <NavigationItem link='/orders'>Orders</NavigationItem>
     </ul>        
     );
